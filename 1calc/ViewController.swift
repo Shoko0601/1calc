d96f5ed35e1d48eb203fc6abe4b330d54214dae0
管理者 <admin@macbookpro.local>
2017-01-22 04:36:55 +0900

    画面変更
    画面変更

--- 1calc/ViewController.swift
+++ 1calc/ViewController.swift
@@ -25,63 +25,67 @@ class ViewController: UIViewController {
 
 
  
+  
     @IBOutlet weak var genjituText: UITextField!
     
-   
-   
-    
-    //genjituText.text = mon
-    
-  //textField.genjituText = UIKeyboardType.Number
-    
-    var mon : Int = 0//モニタ1
-    var a : Int = 0//モニタ1
-    var cal : Int = 0//計算
+         
+    var mon : Double = 0//モニタ1
+    var a : Double = 0//モニタ1
+    var cal : Double = 0//計算
     var enz : Int = 1//演算子の振り分け
+    var han : Int = 0//数字入力判定用
     
     
     @IBAction func but1(sender: AnyObject) {
         a = 1
+        han = 1
          mon = mon*10 + a
         genjituText.text = String(mon)
     }
    
     @IBAction func but2(sender: AnyObject) {
         a = 2
+        han = 1
         mon = mon*10 + a
         genjituText.text = String(mon)
     }
     
     @IBAction func but3(sender: AnyObject) {
         a = 3
+        han = 1
         mon = mon*10 + a
         genjituText.text = String(mon)
     }
     
     @IBAction func but4(sender: AnyObject) {
         a = 4
+        han = 1
         mon = mon*10 + a
         genjituText.text = String(mon)    }
     
     @IBAction func but5(sender: AnyObject) {
         a = 5
+        han = 1
         mon = mon*10 + a
         genjituText.text = String(mon)    }
     
     @IBAction func but6(sender: AnyObject) {
         a = 6
+        han = 1
         mon = mon*10 + a
         genjituText.text = String(mon)
     }
     
     @IBAction func but7(sender: AnyObject) {
         a = 7
+        han = 1
         mon = mon*10 + a
         genjituText.text = String(mon)
     }
     
     @IBAction func but8(sender: AnyObject) {
         a = 8
+        han = 1
         mon = mon*10 + a
         genjituText.text = String(mon)
     }
@@ -89,6 +93,14 @@ class ViewController: UIViewController {
    
     @IBAction func but9(sender: AnyObject) {
         a = 9
+        han = 1
+        mon = mon*10 + a
+        genjituText.text = String(mon)
+    }
+    
+    @IBAction func but0(sender: AnyObject) {
+        a = 0
+        han = 1
         mon = mon*10 + a
         genjituText.text = String(mon)
     }
@@ -98,42 +110,101 @@ class ViewController: UIViewController {
     
     
     
-    
     @IBAction func but(sender: AnyObject) {
-        if enz == 1{
+        if han == 0{
+        }
+        else if enz == 1{
             cal = cal + mon
         }
-        if enz == 2{
+        else if enz == 2{
             cal = cal - mon
         }
-        if mon == 0{
-        }else{
-            enz = 1
-            mon = 0
+        else if enz == 3{
+            cal = cal * mon
         }
+        else if enz == 4{
+            cal = cal / mon
+        }
+        enz = 1
+        mon = 0
+        han = 0
         genjituText.text = String(cal)
     }
     
     @IBAction func butMai(sender: AnyObject) {
-        if enz == 1{
+        if han == 0{
+        }
+        else if enz == 1{
             cal = cal + mon
         }
-        if enz == 2{
+        else if enz == 2{
             cal = cal - mon
         }
-        
-        if mon == 0{
-        }else{
-            enz = 2
-            mon = 0
+        else if enz == 3{
+            cal = cal * mon
         }
+        else if enz == 4{
+            cal = cal / mon
+        }
+        enz = 2
+        mon = 0
+        han = 0
         genjituText.text = String(cal)
     }
+    
+    @IBAction func butKak(sender: AnyObject) {
+        
+        if han == 0{
+        }
+        else if enz == 1{
+            cal = cal + mon
+        }
+        else if enz == 2{
+            cal = cal - mon
+        }
+        else if enz == 3{
+            cal = cal * mon
+        }
+        else if enz == 4{
+            cal = cal / cal
+        }
+        enz = 3
+        mon = 0
+        han = 0
+        genjituText.text = String(cal)
+    }
+
+    @IBAction func butWar(sender: AnyObject) {
+        if han == 0{
+        }
+        else if enz == 1{
+            cal = cal + mon
+        }
+        else if enz == 2{
+            cal = cal - mon
+        }
+        else if enz == 3{
+            cal = cal * mon
+        }
+        else if enz == 4{
+            cal = cal / mon
+        }
+        enz = 4
+        mon = 0
+        han = 0
+        genjituText.text = String(cal)
+        
+    }
+
+ 
+    
+    
     
     @IBAction func butAc(sender: AnyObject) {
         cal = 0
         mon = 0
         enz = 1
+        han = 0
         genjituText.text = String(cal)
     }
 
@@ -148,13 +219,14 @@ class ViewController: UIViewController {
         }else if enz == 2{
             cal = cal - mon
             genjituText.text = String(cal)
+        }else if enz == 3{
+            cal = cal * mon
+            genjituText.text = String(cal)
+        }else if enz == 4{
+            cal = cal / mon
+            genjituText.text = String(cal)
         }
-        
+        print(mon)
+        han = 0
     }
-    
-    
-    
-    
-    
 }
-
